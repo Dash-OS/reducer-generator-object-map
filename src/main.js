5a7288204365ee98756d3a62c213ec3e8ec9a3a6
bradynapier <brady.napier@dashos.net>
2017-06-16 21:47:28 +0000

    allow passing arbitrary args at both steps

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 export default 
-  (initialState, handlers = {}, pcontext) => 
-    (state = initialState, action, context) => 
+  (initialState, handlers = {}, pcontext, ...initialArgs) => (
+    (state = initialState, action, context, ...reducerArgs) => (
       ( ! action || ! action.type || typeof handlers[action.type] !== 'function' )
         ? state
-        : handlers[action.type](state, action, { ...pcontext, ...context })
+        : handlers[action.type](state, action, { ...pcontext, ...context }, ...initialArgs, ...reducerArgs)
+    )
+  )
